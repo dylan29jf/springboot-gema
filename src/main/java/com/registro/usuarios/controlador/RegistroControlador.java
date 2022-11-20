c8e05e29cc097488650c958092aa5bf39a72739b
dylan29jf <dylanjay153@gmail.com>
2022-11-19 19:34:30 -0600

    Spring Boot Social Network

--- src/main/java/com/registro/usuarios/controlador/RegistroControlador.java
+++ src/main/java/com/registro/usuarios/controlador/RegistroControlador.java
@@ -21,6 +21,12 @@ public class RegistroControlador {
 	@GetMapping("/")
 	public String verPaginaDeInicio(Model modelo) {
 		modelo.addAttribute("usuarios", servicio.listarUsuarios());
+		// <tr th:each="usuario : ${usuarios}">
+		// 			<td th:text="${usuario.id}">ID</td>
+		// 			<td th:text="${usuario.nombre}">ID</td>
+		// 			<td th:text="${usuario.apellido}">ID</td>
+		// 			<td th:text="${usuario.email}">ID</td>
+		// 		</tr>
 		return "index";
 	}
 }
